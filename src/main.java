4fa671f8b77886656f3a9585dbd632058b3bb056
Kendrick Torrance <torraken000@warren.k12.in.us>
2025-02-21 08:09:57 -0500

    1st

--- src/main.java
+++ src/main.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class main {
+class main{
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        list.add("Date");
+        list.add("Elderberry");
+
+        list.remove(2);
+
+        Collections.sort(list);
+
+        System.out.println("Modified List " + list);
+    }
 }
+
+
